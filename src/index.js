5c5fabb488428a21fe19293b9729e8631d904643
InhamiRei <1620592237@qq.com>
2024-12-02 15:28:54 +0800

    🔥 Fixed issue # 2024-12-2 15:28
    水印修改名称

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import T_watermark from "./components/waterMark.js";
  * 时间轴入口函数
  * @returns 返回时间轴组件实例
  */
-function TWatermark(config) {
+function TWaterMark(config) {
   return new T_watermark(config);
 }
 
-window.TWatermark = TWatermark;
-export default TWatermark;
+window.TWaterMark = TWaterMark;
+export default TWaterMark;
